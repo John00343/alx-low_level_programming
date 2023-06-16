070b60a1676f63190db77ca06780ef509c91dfa3
John00343 <johnwmacharia8@gmail.com>
2023-06-16 21:19:36 +0000

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
  /**
-  * main - Prints the alphabets.
+  * main - Prints the alphabet.
   *
-  * Return: Always 0 (success)
+  * Return: Always 0 (Success)
   */
 int main(void)
 {

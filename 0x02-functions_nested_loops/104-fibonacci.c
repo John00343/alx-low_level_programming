8ec8e4ae7d6d75d4f69e4570c3befc1ebab8a218
John00343 <johnwmacharia8@gmail.com>
2023-06-20 22:37:23 +0000

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -33,8 +33,8 @@ int main(void)
 
 	for (i = 92; i < 99; ++i)
 	{
-		printf(", %lu", aft1 + (aft2 / l));
-		printf("%lu", aft2 % l);
+		printf(", %lu", aft1 + (aft2 / I));
+		printf("%lu", aft2 % I);
 		aft1 = aft1 + bef1;
 		bef1 = aft1 - bef1;
 		aft2 = aft2 + bef2;

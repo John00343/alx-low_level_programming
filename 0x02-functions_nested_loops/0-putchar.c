ed00a8e474e6f0828905be847cfe73777c3e7a27
John00343 <johnwmacharia8@gmail.com>
2023-06-20 20:50:47 +0000

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <main.h>
+
+/**
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_Putchar('u');
+	_Putchar('t');
+	_Putchar('c');
+	_Putchar('h');
+	_Putchar('a');
+	_Putchar('r');
+	_Putchar("\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *void-print the alphabets in lowercase
+ */
+void print_alphabet(void)
+{
+
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+		_putchar("\n");
+}

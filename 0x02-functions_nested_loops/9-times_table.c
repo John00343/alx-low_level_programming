be997c5ac80b810a4da1a22f8f1bc4b563653357
John00343 <johnwmacharia8@gmail.com>
2023-06-20 21:59:03 +0000

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,7 +14,7 @@ void times_table(void)
 			k = j * i;
 			if (j == 0)
 			{
-				_putchar(k + '0')
+				_putchar(k + '0');
 			}
 			if (k < 10 && j != 0)
 			{
@@ -31,6 +31,6 @@ void times_table(void)
 				_putchar((k % 10) + '0');
 			}
 		}
-		_putchar("\n");
+		_putchar('\n');
 	}
 }

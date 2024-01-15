0297f876aa98e4a52cb4ab0016b71bf152627eb6
john00343 <johnwmacharia8@gmail.com>
2024-01-15 12:58:04 -0800

    a dynamic library that contains C functions that can be called from Python.

---
+++ 0x18-dynamic_libraries/all.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * add - add 2 int
+ * @a: first int
+ * @b: second int
+ * Return: addition
+ */
+int add(int a, int b) {
+    return a + b;
+}
+
+/**
+ * divide - divide 2 int
+ * @a: first int
+ * @b: second int
+ * Return: division
+ */
+int divide(int a, int b) {
+    return a / b;
+}
+
+/**
+ * multiply - multiply 2 int
+ * @a: first int
+ * @b: second int
+ * Return: multiplication
+ */
+int multiply(int a, int b) {
+    return a * b;
+}
+
+/**
+ * modulus - modulus of 2 int
+ * @a: first int
+ * @b: second int
+ * Return: modulus
+ */
+int modulus(int a, int b) {
+    return a % b;
+}
+
+/**
+ * subtract - difference of 2 int
+ * @a: first int
+ * @b: second int
+ * Return: subtraction
+ */
+int subtract(int a, int b) {
+    return a - b;
+}
+
